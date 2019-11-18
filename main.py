32f72509e4ffeb92828bb5c0d47987ff2c09942e
Dhiraj <d.meenavilli@share.epsb.ca>
2019-11-17 20:54:08 -0700

    I now can save things to the text file Log.txt so now I just need events to occur so I can figure out how I save them.

--- main.py
+++ main.py
@@ -3,33 +3,43 @@ Date: 11/17/2019
 Authour: Moataz Khallf & Dhiraj Meenavilli
 Title: Fantasy Tracker
 """
+Log = 'Log.txt'
+"""
+try:
+    open(Log,'x')
 
+except:
+    open(Log,'r')
+"""
 ### ------------------------------ Quest Tracker --------------------------------- ###
 class Quest:
-	def __init__(description, goal, reward):
-		self.des = description
-		self.goal = goal
-		self.reward = reward
+    def __init__(self,description, goal, reward):
+        self.des = description
+        self.goal = goal
+        self.reward = reward
 
-	def get_des(self):
-		return self.des
+    def get_des(self):
+        return self.des
 
-	def get_goal(self):
-		return self.goal
+    def get_goal(self):
+        return self.goal
 
-	def get_reward(self):
-		return self.reward
+    def get_reward(self):
+        return self.reward
 
-	def set_goal(self,new_goal):
-		self.goal = new_goal
+    def set_goal(self,new_goal):
+        self.goal = new_goal
 
-	def set_reward(self,new_reward):
-		self.reward = new_reward
+    def set_reward(self,new_reward):
+        self.reward = new_reward
 ### ------------------------------ Combat System --------------------------------- ###
 
 ### ------------------------------ Charachter ------------------------------------ ###
 
 ### ------------------------------ Summary Log ----------------------------------- ###
 
-def log(event):
+#def log(event):
+f = open(Log, "w")
+f.write("Now the file has more content!")
+f.close()
 
