2ea8186138694342ac1ce50f957e0223566f0c3f
Moataz Khallaf <48331207+taz717@users.noreply.github.com>
2020-01-30 11:00:09 -0700

    NPC Class started, atrributes are finished I'm prety sure, also if self.*insertstathere* is done for parent class too

--- Library.py
+++ Library.py
@@ -1,6 +1,6 @@
 '''
 Moataz Khallaf
-Character classes
+Library
 2020/01/28
 
 Contributors: Dhiraj Meenavilli -> Hit function
@@ -9,6 +9,8 @@ Contributors: Dhiraj Meenavilli -> Hit function
 
 import pandas as pd
 import random
+
+# _____________ Classes _______________
 
 class Character:
     """ 
@@ -31,7 +33,20 @@ class Character:
             if stat_lsit['ID'][unit] == id:
                 self.id = id
                 self.stats = stat_lsit.iloc[unit]
+                self.race = self.stats['RACE']
+                self.tag = self.stats['TAG']
                 self.hp = self.stats['HP']
+                self.max_hp = self.stats['MAXHP']
+                self.dmg = self.stats['DMG']
+                self.acc = self.stats['ACC']
+                self.spd = self.stats['SPD']
+                self.dodge = self.stats['DODGE']
+                self.prot = self.stats['PROT']
+                self.bleed_res = self.stats['BleedRES']
+                self.blight_res = self.stats['BlightRES']
+                self.move_res = self.stats['MoveRES']
+                self.stun_res = self.stats['StunRES']
+                self.actions = self.stats['Actions']
 
     def get_unit_stats(self):
         """
@@ -80,13 +95,48 @@ class Character:
     def __str__(self):
         return str(self.stats.values.tolist())
 
+class NPC(Character):
+    def __init__(self, id, stat_lsit):
+        Character.__init__(self, id, stat_lsit)
+        self.type = self.stats['Type']
+        self.basic = self.stats['Basic']
+        self.blight = self.stats['Blight']
+        self.blight_chance = self.stats['Blight%Chance']
+        self.blight_dmg_mod = self.stats['blightDMGMod%']
+        self.blight_dot = self.stats['blightDOT']
+        self.bleed = self.stats['Bleed']
+        self.bleed_chance = self.stats['Bleed%Chance']
+        self.bleed_dmg_mod = self.stats['bleedDMGMod%']
+        self.bleed_dot = self.stats['bleedDOT']
+        self.stun = self.stats['Stun']
+        self.stun_chance = self.stats['Stun%Chance']
+        self.stun_dmg_mod = self.stats['stunDMGMod']
+        self.move = self.stats['Move']
+        self.move_chance = self.stats['Move%Chance']
+        self.move_dmg_mod = self.stats['moveDMGMod']
+
+
+
+# _____________ Functions _______________
+
+def hit(acc, dodge):
+    acc = random.randrange(acc, 100)
+    dodge = random.randrange(dodge, 100)
+    if acc - dodge >= 1:
+        itHit = 1
+    else:
+        itHit = 0
+
+    return itHit
+
+# _____________ TEST _______________
 
 if __name__ == "__main__":
     import pandas as pd
     unit_stats = pd.read_csv('unitStats.csv')
     test_repr = []
     for i in range(0, 16):
-        test_repr.append(Character(i, unit_stats))
+        test_repr.append(NPC(i, unit_stats))
     for i in range(len(test_repr)):
         print(test_repr[i])
     print(test_repr[0].get_unit_stats().head())
